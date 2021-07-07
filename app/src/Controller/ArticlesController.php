1a9be568a09136469224d2b6aa0ce3c9db5478d0
Impartiall <jmlooney64@gmail.com>
2021-07-07 11:47:38 -0400

    Implement project from tutorial

---
+++ app/src/Controller/ArticlesController.php
@@ -0,0 +1,109 @@
+<?php
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
+    public function index()
+    {
+        $this->Authorization->skipAuthorization();
+
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
+
+    public function view($slug = null)
+    {
+        $this->Authorization->skipAuthorization();
+
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->contain('tags')
+            ->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        $this->Authorization->authorize($article);
+
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            $article->user_id = $this->request->getAttribute('identity')->getIdentifier();
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+
+        $tags = $this->Articles->Tags->find('list')->all();
+
+        $this->set(compact('article', 'tags'));
+    }
+
+    public function edit($slug)
+    {
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->contain('Tags')
+            ->firstOrFail();
+        $this->Authorization->authorize($article);
+
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData(), [
+            'accessibleFields' => ['user_id' => false]
+            ]);
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+
+        $tags = $this->Articles->Tags->find('list')->all();
+
+        $this->set(compact('article', 'tags'));
+    }
+
+    public function delete($slug)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->Authorization->authorize($article);
+
+        if ($this->Articles->delete($article))
+        {
+            $this->Flash->success(__('The {0} article has been deleted.', $article->title));
+            return $this->redirect(['action' => 'index']);
+        }
+    }
+
+    public function tags(...$tags)
+    {
+        $this->Authorization->skipAuthorization();
+
+        $articles = $this->Articles->find('tagged', [
+            'tags' => $tags
+        ])
+        ->all();
+
+        $this->set([
+            'articles' => $articles,
+            'tags' => $tags
+        ]);
+    }
+}
